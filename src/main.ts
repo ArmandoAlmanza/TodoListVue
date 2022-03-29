1627092d1f9a177244e6347ca9d42e440a9b17c8
Armando Almanza <armando.s.almanza@gmail.com>
2022-03-29 17:32:52 -0600

    add router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './styles/app.scss'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./styles/app.scss";
+import router from "./routes/";
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount("#app");
